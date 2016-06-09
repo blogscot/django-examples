e0c1efdbc17ec026a0d3241f1373edf2810cdb76
blogscot <iain.diamond@gmail.com>
2016-06-09 17:40:18 +0100

    Generic Foreign Keys

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
 from django.db.models import Q
@@ -6,6 +7,8 @@ from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
+
+from comments.models import Comment
 from .models import Post
 from .forms import PostForm
 
@@ -34,8 +37,13 @@ def post_detail(request, slug):
         if not user.is_staff or not user.is_superuser:
             raise Http404
 
+    content_type = ContentType.objects.get_for_model(Post)
+    # Retrieve the comments for the given post
+    comments = Comment.objects.filter(content_type=content_type, object_id=post.id)
+
     context = {
         'post': post,
+        'comments': comments,
     }
     return render(request, 'posts/detail.html', context)
 
