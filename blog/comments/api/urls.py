e5d2f91fcd939df9deaafedd8e8a0e97ad94f065
blogscot <iain.diamond@gmail.com>
2016-06-16 17:50:17 +0100

    Comment create serializer

--- blog/comments/api/urls.py
+++ blog/comments/api/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 
-from .views import \
-    (CommentListAPIView,
+from .views import (
+     CommentListAPIView,
+     CommentCreateAPIView,
      CommentRetrieveAPIView,
-     # CommentDeleteAPIView,
      )
 
 urlpatterns = [
     url(r'^$', CommentListAPIView.as_view(), name='list'),
+    url(r'^create/$', CommentCreateAPIView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', CommentRetrieveAPIView.as_view(), name='thread'),
     # url(r'^(?P<id>\d+)/delete/$', CommentDeleteAPIView.as_view(), name='delete'),
 ]
--- blog/comments/api/views.py
+++ blog/comments/api/views.py
@@ -26,16 +26,28 @@ from posts.api.pagination import \
 from posts.api.permissions import IsOwnerOrReadOnly
 
 from ..models import Comment
-from .serializers import (CommentSerializer, CommentDetailSerializer)
+from .serializers import (
+    CommentSerializer,
+    CommentDetailSerializer,
+    create_comment_serializer,
+)
 
 
-# class PostCreateAPIView(CreateAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostCreateSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
+class CommentCreateAPIView(CreateAPIView):
+    queryset = Comment.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        model_type = self.request.GET.get('type')
+        slug = self.request.GET.get('slug')
+        parent_id = self.request.GET.get('parent_id', None)
+
+        return create_comment_serializer(
+            model_type=model_type,
+            slug=slug,
+            parent_id=parent_id,
+            user=self.request.user,
+        )
 
 
 class CommentRetrieveAPIView(RetrieveAPIView):
